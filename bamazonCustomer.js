5c72d44da828c57fc680c66f0686a8ee2bcb3ed7
Trent Marshall <Trent@lawn-128-61-56-43.lawn.gatech.edu>
2017-04-20 21:15:12 -0400

    v1.0

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,8 @@
+//dependencies
 var mysql = require('mysql');
 var prompt = require('prompt');
-var $ = require('jQuery');
 
+//connect to the mysql server
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -13,18 +14,23 @@ var connection = mysql.createConnection({
   password: "basketball",
   database: "top_songsDB"
 });
-
+//if we are connected, run showInventory
 connection.connect(function(err) {
   if (err) throw err;
   showInventory();
 });
 
+//start the app
 var showInventory = function() {
+	//query the table and display all products in a somewhat
+	//orderly manner (this could be done better)
 	var query = "SELECT * FROM Bamazon.products;"
+	//query the database
 	connection.query(query, function(err, res){
 		for (var i = 0; i <res.length; i++) {
 			console.log("Item #" + res[i].item_id + " || " + res[i].product_name + " || " + res[i].department_name + " || " + res[i].price + " || " + res[i].stock_quantity + " units in stock.");
 		}
+	//begin the prompt
 	prompt.start();
 
 	prompt.get([{
@@ -38,8 +44,10 @@ var showInventory = function() {
 		type: 'integer',
 		required: true
 		}], function (err, result) {
+			//capture the results into variables we can use
 			var itemID = result.item;
 			var itemQuantity = result.quantity;
+			//build our new query
 			query = "SELECT * FROM Bamazon.products WHERE item_id=" + itemID;
 			connection.query(query, function(err, res) {
 				var qty = res[0].stock_quantity;
@@ -50,14 +58,17 @@ var showInventory = function() {
 				}
 				else {
 					var totalPrice = itemQuantity * price;
+					//convert to a fixed decimal price
 					var decimalPrice = totalPrice.toFixed(2);
 					var newQuantity = qty - itemQuantity;
+					//log the results
 					console.log("---------------------------");
 					console.log('Your order has been placed.');
 					console.log("---------------------------");
 					console.log("Your total price: " + decimalPrice);
 					console.log("---------------------------");
 					console.log("Thank you, come again! Press control+c to exit.");
+					//make the update to the table
 					connection.query("UPDATE Bamazon.products SET ? WHERE ?", [{
 						stock_quantity: newQuantity
 						}, {
