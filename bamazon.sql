23afec2e863a868d5f226a66995f694138fe926c
Trent Marshall <Trent@Sarahs-MacBook-Air-2.local>
2017-04-22 00:21:58 -0400

    v1.2

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,33 @@ CREATE TABLE products (
     PRIMARY KEY (item_id)
 
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sip of Sunshine IPA", "beer", 6.99, 32);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pliny the Elder", "beer", 12.99, 24);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Michter's Celebration", "bourbon", 4999.99, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pappy van Winkle 20 year", "bourbon", 259.99, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("George T Stagg", "bourbon", 129.99, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("WL Weller 12 year", "bourbon", 26.99, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Eagle Rare", "bourbon", 29.99, 24);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Four Roses Single Barrel", "bourbon", 62.99, 64);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Focal Banger", "beer", 4.99, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Heady Topper", "beer", 5.99, 12);
